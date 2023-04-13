ab3cab5f692180e0545103d689900754113da3b7
yagami1995 <yavier04271995@gmail.com>
2023-04-13 15:59:42 -0500

    este es el segundo commit

--- app/src/main/java/com/yagami/movilcamellapp/Manualconstruayuda.java
+++ app/src/main/java/com/yagami/movilcamellapp/Manualconstruayuda.java
@@ -9,6 +9,9 @@ import android.view.View;
 
 public class Manualconstruayuda extends AppCompatActivity {
 CardView card_view;
+CardView card_view2;
+CardView card_view3;
+CardView card_view4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +24,33 @@ CardView card_view;
                 startActivity(card_view);
             }
         });
+        card_view2 = findViewById(R.id.card_view2);
+        card_view2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent card_view2 = new Intent(Manualconstruayuda.this,Ceramica.class);
+                startActivity(card_view2);
+            }
+        });
+        card_view3 = findViewById(R.id.card_view3);
+        card_view3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent card_view3= new Intent(Manualconstruayuda.this,Electri.class);
+                startActivity(card_view3);
+            }
+        });
+        card_view4 = findViewById(R.id.card_view4);
+        card_view4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent card_view4= new Intent(Manualconstruayuda.this,Techo.class);
+                startActivity(card_view4);
+            }
+        });
+
+
+
 
     }
 
@@ -32,4 +62,18 @@ CardView card_view;
     }
 
 
+    public void comoinstalarcera(View view) {
+        Intent funcion=new Intent(this,Ceramica.class);
+        startActivity(funcion);
     }
+
+    public void Electri(View view) {
+        Intent funcion=new Intent(this,Electri.class);
+        startActivity(funcion);
+    }
+
+    public void Techo(View view) {
+        Intent funcion=new Intent(this,Techo.class);
+        startActivity(funcion);
+    }
+}
